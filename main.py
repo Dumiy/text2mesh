78fc59c5356d94c11fba2d7e6e435f2d93eacc06
Dumiy <45998406+Dumiy@users.noreply.github.com>
2022-01-12 17:18:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -448,7 +448,7 @@ if __name__ == '__main__':
     parser.add_argument('--frontview', action='store_true')
     parser.add_argument('--no_prompt', default=False, action='store_true')
     parser.add_argument('--exclude', type=int, default=0)
-    parser.add_argument('--iter_save', default=100, action='store_true')
+    parser.add_argument('--iter_save', type=int, default=100)
 
     parser.add_argument('--frontview_std', type=float, default=8)
     parser.add_argument('--frontview_center', nargs=2, type=float, default=[0., 0.])
